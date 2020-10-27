e5bbdc7750600964511533d47386bdd8a441a980
LeeJinWoo-web <aoel900202@gmail.com>
2020-10-27 12:08:45 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import React, { Component } from "react";
+import TOC from './components/TOC';
+import Content from './components/Content';
+import Subject from './components/Subject';
 import './App.css';
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-          <li><a href="1.html">html</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript11111</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-      <article>
-        <h2>HTML</h2>
-        HTML is Hyper Text Markup Language.      
-      </article>
-    );
-  }
-}
-
-class Subject extends Component{
-  render(){
-    return(
-      <header>
-        <h1>WEB</h1>
-        world wide web!
-      </header>
-    );
-  }
-}
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Subject/>
+        <Subject title="WEB" sub="world wide web!"/>
         <TOC/>
-        <Content/>
+        <Content title="HTML" desc="HTML is Hyper Text Markup Language."/>
       </div>
     );
   }
