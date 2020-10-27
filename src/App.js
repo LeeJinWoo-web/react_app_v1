070dcd36a500eddf749abc5a241860d50fe84d46
LeeJinWoo-web <aoel900202@gmail.com>
2020-10-27 13:45:28 +0900

    master

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Subject from './components/Subject';
 import './App.css';
 
 class App extends Component {
+
   //컴포넌트가 실행될때 constructor 제일먼저 실행된다
   constructor(props){
     super(props);
     this.state = {
+      mode: "read",
       subject:{title:"WEB", sub:"world wide web!!"},
+      welcome:{title:"Welcome", desc:"Hello,React!!"},
       contents:[
         {id: 1, title: "HTML", desc: "HTML is Imformation"},
         {id: 2, title: "REACT", desc: "CSS is Design"},
@@ -18,18 +21,43 @@ class App extends Component {
     }
   }
 
+  _clcikEvent = () =>{
+    console.log(1)
+    this.setState({
+      mode: "welcome"
+    })
+  }
+
   render(){
+    let _title, _desc = null;
+    if(this.state.mode === "welcome"){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;    
+    } else if(this.state.mode === "read"){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;  
+    }
     return (
       <div className="App">
-        <Subject 
-          title={this.state.subject.title} 
-          sub={this.state.subject.sub}
-        />
+        <header>
+          <h1>
+            <a 
+              href="" 
+              onClick={ e => {
+                  e.preventDefault();
+                  this._clcikEvent()
+              }}>
+              {this.state.subject.title}
+             </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}/>
-        <Content title="HTML" desc="HTML is Hyper Text Markup Language."/>
+        <Content title={_title} desc={_desc}/>
       </div>
     );
   }
+
 }
 
 export default App;
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -4,7 +4,7 @@ class Subject extends Component{
     render(){
       return(
         <header>
-          <h1>{this.props.title}</h1>
+          <h1><a href="" onClick={ e => { e.preventDefault(); alert(1); }}>{this.props.title}</a></h1>
           {this.props.sub}
         </header>
       );
