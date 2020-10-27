49049f327c94953e6a953b4869a495a8b003fe03
LeeJinWoo-web <aoel900202@gmail.com>
2020-10-27 10:48:40 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class TOC extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li><a href="1.html">html</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+        <h2>HTML</h2>
+        HTML is Hyper Text Markup Language.      
+      </article>
+    );
+  }
+}
+
+class Subject extends Component{
+  render(){
+    return(
+      <header>
+        <h1>WEB</h1>
+        world wide web!
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Subject/>
+        <TOC/>
+        <Content/>
+      </div>
+    );
+  }
 }
 
 export default App;
