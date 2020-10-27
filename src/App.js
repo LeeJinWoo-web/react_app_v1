d321f298614cc919976a7f4969a4ac92340c0df0
LeeJinWoo-web <aoel900202@gmail.com>
2020-10-27 11:03:52 +0900

    1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class TOC extends Component{
         <ul>
           <li><a href="1.html">html</a></li>
           <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript11</a></li>
+          <li><a href="3.html">JavaScript11111</a></li>
         </ul>
       </nav>
     );
